2e7899b74a738aafa81bdbd028ca92ae5e58fbbb
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-06-29 08:40:13 +0000

    [add]finish customers show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fafbc7004a1d67b6173bd8e29436f2d1ea4c903bc2b9b9cc5694b9e32a72bf0f7fe1e80c4cb2b9d29afbd0b8c3c17bdf4a533ae154e3091c48a644cd9a5246f0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  namespace :admin do
+    resources :customers, except: [:new, :create, :destroy]
+  end
+  namespace :admin do
+    resources :genres, except: [:new, :show, :destroy]
+  end
+  namespace :admin do
+    resources :items, except: [:destroy]
+  end
+  namespace :admin do
+    get '/' => "homes#top", as: 'top'
+  end
+  
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions'
+  }
+  
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
