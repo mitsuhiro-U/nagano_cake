f20f0fb549daa06e4189bf558779891e6fa3df9f
mitsuhiro-U <mmm.udagawa@gmail.com>
2021-06-29 11:53:43 +0000

    devise controller,view 作成前の保険

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,16 @@
+class Public::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def withdrawal
+  end
+
+  def withdrawal_update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,34 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/verification'
+    get 'orders/thanks'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/withdrawal'
+    get 'customers/withdrawal_update'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   namespace :admin do
     get '/' => "homes#top", as: 'top'
     resources :customers, except: [:new, :create, :destroy]
